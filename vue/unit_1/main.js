a7fb65a64ad38922b1054a4f590bcc83cf6ded4b
Jarek <t0x3e8@gmail.com>
2019-10-07 23:04:41 +0200

    working solution v1

--- vue/unit_1/main.js
+++ vue/unit_1/main.js
@@ -1,6 +1,17 @@
 new Vue({
   el: "#app",
   data: {
-    msg: "hello world from Vue"
+    header: "List of books:",
+    books: [
+      { title: "The God father" },
+      { title: "Bible" }
+    ],
+    newBook: ""
+  },
+  methods: {
+    addBook: function() {
+      this.books.push({ title: this.newBook });
+      this.newBook = "";
+    }
   }
 });
