dd530ddd8c770e5a4c851c037aaf899160739d23
Jarek <t0x3e8@gmail.com>
2019-11-11 23:37:46 +0100

    form of pet

--- vue/unit_4/multipage_spa/src/views/Pets.vue
+++ vue/unit_4/multipage_spa/src/views/Pets.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
-    This is the {{ $route.params.species }} of index {{ $route.params.id }}
-    {{ pet}}
+    <div>
+      <b-form >
+        <b-form-group id="input-group-1" label="Name:" label-for="input-1" description="Pet's name">
+          <b-form-input id="input-1" v-model="pet.name" type="text" disabled></b-form-input>
+        </b-form-group>
+        <b-form-group id="input-group-2" label="Mood:" label-for="input-2">
+          <b-form-select id="input-2" v-model="pet.mood" :options="moods" disabled />
+        </b-form-group>
+        <b-form-group id="input-group-3" label="Age:" label-for="input-3">
+          <b-form-input id="input-3" v-model="pet.age" type="number" disabled></b-form-input>
+        </b-form-group >
+        <b-form-group id="input-group-4" label="Length:" label-for="input-4">
+          <b-form-input id="input-4" v-model="pet.length" type="number" disabled></b-form-input>
+        </b-form-group>
+        <b-form-group id="input-group-5" disabled>
+          <b-form-checkbox v-model="pet.available">Is it available for adoption?</b-form-checkbox>
+        </b-form-group>
+      </b-form>
+    </div>
   </div>
 </template>
 
@@ -11,17 +28,16 @@ import { mapState } from 'vuex'
 export default {
   data () {
     return {
-      pet: {}
+      pet: {},
+      moods: []
     }
   },
   computed: {
-    ...mapState([
-      'cats',
-      'dogs'
-    ])
+    ...mapState(['cats', 'dogs'])
   },
   mounted () {
     this.pet = this[this.$route.params.species][this.$route.params.id]
+    this.moods.push(this.pet.mood)
   }
 }
 </script>
